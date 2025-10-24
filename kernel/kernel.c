ce2935e4d5b4a1e83e39d00fd1943d82ada19263
Muhammad36861 <m.asharwaseem@gmail.com>
2025-10-24 14:31:39 +0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,31 @@
+#include <stdint.h>
+
+#define VGA_WIDTH 320
+#define VGA_HEIGHT 200
+#define VGA_ADDRESS 0xA0000
+
+void putpixel(int x, int y, uint8_t color) {
+    uint8_t* VGA = (uint8_t*)VGA_ADDRESS;
+    VGA[(y * VGA_WIDTH) + x] = color;
+}
+
+void clear_screen(uint8_t color) {
+    for (int y = 0; y < VGA_HEIGHT; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
+            putpixel(x, y, color);
+}
+
 void kernel_main(void) {
-    char *video = (char*)0xb8000;
-    const char *msg = "Hexa OS Kernel Initialized";
-    int i = 0;
-    while (msg[i]) {
-        video[i * 2] = msg[i];
-        video[i * 2 + 1] = 0x0F;   // white on black
-        i++;
-    }
-    for(;;);  // halt
+    clear_screen(1); // Blue background
+    // draw taskbar
+    for (int y = 180; y < 200; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
+            putpixel(x, y, 8); // Gray taskbar
+
+    // start button
+    for (int y = 182; y < 198; y++)
+        for (int x = 5; x < 45; x++)
+            putpixel(x, y, 2); // Green Start
+
+    for(;;);
 }
